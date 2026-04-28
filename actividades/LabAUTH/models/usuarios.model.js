a8e5cf8186847f8d074502fd2cd842fbd8b9f221
FacundoGPG <fakgpineirog@gmail.com>
2026-04-28 12:14:04 -0600

    added Lab and activity

---
+++ actividades/LabAUTH/models/usuarios.model.js
@@ -0,0 +1,25 @@
+const pool = require('../util/database');
+
+exports.User = class {
+    constructor(username, name, password){
+        this.name = name;
+        this.password = password;
+    }
+
+    async save(){
+        const hashedPass = await bcrypt.hash(this.password, 12);
+        const sql = `INSERT INTO users (username, name, password) 
+                    VALUES ($1, $2, $3)
+                    RETURNING id, username, name`;
+        const { rows } = await pool.query(sql, [this.username, this.name, this.password]);
+        return rows[0];
+    }
+
+    static async findByUsername(username){
+        const sql = `SELECT id, username, password 
+                    FROM users 
+                    WHERE username = $1`;
+        const { rows } = await pool.query(sql, [username]);
+        return rows[0] || null;
+    }
+};
