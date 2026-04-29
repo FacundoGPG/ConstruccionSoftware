8d5946ec490206a4b66dfbdcad5dafa6b188ca23
FacundoGPG <fakgpineirog@gmail.com>
2026-04-28 22:07:17 -0600

    added text in LABMVC

--- actividades/LABMVC/routes/usuarios.routes.js
+++ actividades/LABMVC/routes/usuarios.routes.js
@@ -1,21 +1,10 @@
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const path    = require('path');
+const fs      = require('fs');
 const router = express.Router();
-const controllerUsuarios = require('../controllers/usuarios.controller');
+const controller = require("../controllers/usuarios.controller.js")
 
-router.get("/health", (req, res) => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).json({ status: "ok Usuarios" });
-    res.end();
-});
 
-router.get("/obtener_usuarios", controllerUsuarios.getAllUsers);
-router.get("/obtener_usuarios_activos", controllerUsuarios.getAllUsersActivos);
-router.get("/agregar_usuario", controllerUsuarios.addUserView);
-router.post("/agregar_usuario", controllerUsuarios.addUserForm);
-router.get("/editar_usuario", controllerUsuarios.editUserView);
-router.post("/editar_usuario", controllerUsuarios.editUserForm);
-router.post("/eliminar_usuario", controllerUsuarios.deleteUser);
+router.get('/obtener_usuarios', controller.index);
 
 module.exports = router;
