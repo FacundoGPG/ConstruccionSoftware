2c084df1dbd609b7614df607ebaed6492cb9236f
FacundoGPG <fakgpineirog@gmail.com>
2026-05-03 19:48:47 -0600

    updated claseExpress with class notes from docosaurio

--- actividades/ClaseExpress/index.js
+++ actividades/ClaseExpress/index.js
@@ -1,27 +1,63 @@
+const http    = require('http');
 const express = require('express');
-const fs      = require('fs');
 const path    = require('path');
-const router = express.Router();
+const fs      = require('fs');
+const app     = express();
 
-router.get('/form_method', (request, response, next) => {
-    response.setHeader('Content-Type', 'text/html');
-    const html = fs.readFileSync(path.resolve(__dirname, './form.html'), 'utf8')
-    response.write(html);
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.get('/', (request, response, next) => {
+    response.setHeader('Content-Type', 'text/plain');
+    response.send("URL index /");
+    response.end(); 
+});
+
+app.get('/test_json', (request, response, next) => {
+    response.setHeader('Content-Type', 'application/json');
+    response.json({code:200, msg:"Ok GET"});
     response.end();  
 });
 
-router.post('/form_method', (request, response, next) => {
-    const indice = Number(request.body.indice);
-    const imprimir = request.body.imprimir;
-
-    for(var i = 1; i <= indice; i++){
-        console.log(imprimir)
-    }
-
+app.post('/test_json', (request, response, next) => {
     response.setHeader('Content-Type', 'application/json');
-    response.statusCode = 200;
-    response.write('{code:200, msg:"Ok POST"}');
-    response.end();
+    response.json({code:200, msg:"Ok POST"});
+    response.end();  
 });
 
-module.exports = router;
+app.get('/test_html', (request, response, next) => {
+    response.setHeader('Content-Type', 'text/html');    
+    response.write(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>Código en HTML</title>
+        </head>
+        <body>
+            <h1>hola mundo desde express</h1>
+        </body>
+        </html>
+    `);
+    response.end(); 
+});
+
+const rutasFormulario = require('./routes/formulario.routes');
+app.use('/formulario', rutasFormulario);
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    response.status(404);
+    response.send('¡Page Not Found!'); //Manda la respuesta
+});
+
+const server = http.createServer( (request, response) => {    
+    console.log(request.url);
+});
+app.listen(3000);
