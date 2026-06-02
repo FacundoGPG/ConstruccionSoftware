89dd11ba45c95a7a9718ac8584d39407a490b736
FacundoGPG <fakgpineirog@gmail.com>
2026-06-01 19:25:19 -0600

    debug al LABSUBIRYBAJAR

--- actividades/LABSubirYBajar/index.js
+++ actividades/LABSubirYBajar/index.js
@@ -9,11 +9,12 @@ const log = console.log;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const controller = require ("./index.controller.js");
+app.post('/upload_file', controller.upload_file);
+app.get('/get_private_file/:file', controller.get_private_file);
+
+
 app.listen(port, () => {
     // server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`);
 });
-
-const controller = require ("./index.controller.js");
-app.post('/upload_file', controller.upload_file);
-app.get('/get_private_file/:file', controller.get_private_file);
